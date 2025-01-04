5c7b0b904b4f205e121e9b5bb3eaef11fae7e661
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2025-01-04 09:47:23 +0000

    add

--- IkhlasBond/post/models.py
+++ IkhlasBond/post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    post_description = models.TextField()
+    post_image = models.ImageField(default=None)
+    post_like = models.IntegerField(default=0)
+    post_comments = models.TextField()
+
+    def __str__(self):
+        return self.post_description

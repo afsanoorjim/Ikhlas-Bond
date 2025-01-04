a5ef8af3b8a85e681b52d174d94ca91d18bf31f3
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2025-01-04 08:55:51 +0000

    add

--- IkhlasBond/IkhlasBond/views.py
+++ IkhlasBond/IkhlasBond/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponse
+
 
 def landing(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User(username=name, email=email, password=password)
+        user.save()
+        messages.success(request,"User Created Successfully")
+        return render(request, 'landing.html')
     return render(request, 'landing.html')
